7bc9dcaa2fcdedf2ff9201d7ebd8b830fe63ba21
Fuyuki <ocs.suzuki.f@gmail.com>
2017-11-30 14:42:10 +0900

    image

--- monolog_app/config/routes.rb
+++ monolog_app/config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :microposts,          only: [:create, :destroy]
   resources :likes,       only: [:create, :destroy]
+
 end
